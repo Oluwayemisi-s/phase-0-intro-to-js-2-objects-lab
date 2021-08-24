bc8a97e42e1b0085276d649467ababed3b066da8
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-08-24 12:35:50 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {}
+employee.name = 'Oluwayemisi Soremekun'
+employee.streetAddress = '10532 Court Ave'
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const updateEmployee = Object.assign({},employee,{[key]:value})
+    return updateEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    return Object.assign(employee,{[key]:value})
+}
+
+function deleteFromEmployeeByKey(employee,key){
+    const updateEmployee = {...employee}
+    delete updateEmployee[key]
+    return updateEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key]
+    return employee
+}
